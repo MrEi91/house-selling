6a011534a33a10fd4182b3af1cb5ef1c5f7de471
Eri Irawan <email": null>
2017-03-21 13:34:27 +0700

    add model house

---
+++ server/models/house.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+require('../config/db')
+
+let houseSchema = new Schema({
+  title: String,
+  address: {
+    type: String,
+    required: true
+  },
+  square_feet: Number,
+  num_bedrooms: Number,
+  num_baths: Number,
+  num_floor: Number,
+  cost: Number,
+  sold: Boolean,
+  slug: String,
+  img: String
+}, {
+  timestamps: true
+})
+
+let house = mongoose.model('house', houseSchema)
+
+module.exports = house
